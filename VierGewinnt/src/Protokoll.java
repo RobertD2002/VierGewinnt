ba5ae1ef0f17186753b4a6e7bafa47e773ab1536
RobertD2002 <darminow.robert02@gmail.com>
2021-12-10 10:25:32 +0100

    Fortsetztung

--- VierGewinnt/src/Protokoll.java
+++ VierGewinnt/src/Protokoll.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Protokoll {
-
-    public  Protokoll(){
-
-    }
-
+        private static final Scanner lesen = new Scanner(System.in);
+        public static Spieler spieleranlegen(){
+            Spieler p = null;
+            String name;
+            System.out.println("Spieler Namen eingeben:");
+            name = lesen.next();
+            return p = new Spieler(name, false);
+        }
+        public static Stein[][] spielBrettErzeugen(){
+            Modus modus = new Modus();
+            Stein[][] Brett = modus.feld;
+            return Brett;
+        }
+        public static void brettAusgeben(Stein[][] brett) {
+            for(int i = 0; i < brett.length; i++) {
+                for(int j = 0; j < brett[i].length; j++) {
+                    System.out.print(brett[i][j]+"\t");
+                }
+                System.out.println();
+            }
+        }
 }
 
--- VierGewinnt/src/Spiel.java
+++ VierGewinnt/src/Spiel.java
@@ -4,51 +4,20 @@ import java.util.Scanner;
 public class Spiel {
     private static  final Scanner lesen = new Scanner(System.in);
     public static void main(String[] args){
-        Spieler p1 = spieleranlegen();
-        Spieler p2 = spieleranlegen();
+        Spieler p1 = Protokoll.spieleranlegen();
+        Spieler p2 = Protokoll.spieleranlegen();
         p1.setCurrent(true);
-        Stein s1 = steinAnlegenSpieler1();
-        Stein s2 = steinAnlegenSpieler2();
-        Stein[][] brett = spielBrettErzeugen();
+        Stein s1 = new Stein("\u001B[34m",'○');
+        Stein s2 = new Stein("\u001B[31m",'○');
+        Stein[][] brett = Protokoll.spielBrettErzeugen();
         brett[1][2] = s1;
+        brett[1][3] = s1;
+        brett[1][4] = s1;
+        brett[1][5] = s1;
         brett[3][5] = s1;
         brett[4][5] = s1;
         brett[4][2] = s2;
-
-        brettAusgeben(brett);
-
+        Protokoll.brettAusgeben(brett);
     }
-
-    public static void brettAusgeben(Stein[][] brett) {
-        for(int i = 0; i < brett.length; i++) {
-            for(int j = 0; j < brett[i].length; j++) {
-                System.out.print(brett[i][j]+"\t");
-            }
-            System.out.println();
-        }
-    }
-
-    public static Stein steinAnlegenSpieler1(){
-        Stein stein = new Stein("\u001B[34m",'○');
-        return stein;
-    }
-    public static Stein steinAnlegenSpieler2(){
-        Stein stein = new Stein("\u001B[31m",'○');
-        return stein;
-    }
-
-    public static Spieler spieleranlegen(){
-        Spieler p = null;
-        String name;
-        System.out.println("Spieler Namen eingeben:");
-        name = lesen.next();
-        return p = new Spieler(name, false);
-    }
-    public static Stein[][] spielBrettErzeugen(){
-        Modus modus = new Modus();
-        Stein[][] Brett = modus.feld;
-        return Brett;
-    }
-
 }
 
